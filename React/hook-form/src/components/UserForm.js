f9c3e179e1e4d641110a619cb68568282a68cbf4
Eric Vuu <ericlv91@gmail.com>
2021-03-10 14:07:30 -0800

    more forms assignment

--- React/hook-form/src/components/UserForm.js
+++ React/hook-form/src/components/UserForm.js
@@ -13,26 +13,53 @@ const UserForm = (props) => {
     };
 
     return (
-        <form>
+      <form>
         <div>
           <label htmlFor="firstName">First Name: </label>
           <input onChange={onChange} type="text" name="firstName" />
+          <p>
+            {inputs.firstName.length > 0 && inputs.firstName.length < 2
+              ? "First name must be at least 2 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label htmlFor="lastName">Last Name: </label>
           <input onChange={onChange} type="text" name="lastName" />
+          <p>
+            {inputs.firstName.length > 0 && inputs.firstName.length < 2
+              ? "Last name must be at least 2 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label htmlFor="email">Email Address: </label>
           <input onChange={onChange} type="text" name="email" />
+          <p>
+            {inputs.email.length > 0 && inputs.email.length < 5
+              ? "Email must be at least 5 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label htmlFor="password">Password: </label>
           <input onChange={onChange} type="text" name="password" />
+          <p>
+            {inputs.password.length > 0 && inputs.password.length < 8
+              ? "Password must be at least 8 characters."
+              : ""}
+          </p>
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password: </label>
           <input onChange={onChange} type="text" name="confirmPassword" />
+          <p>
+            {inputs.confirmPassword.length > 0 &&
+            inputs.confirmPassword.length < 8
+              ? "Password must be at least 8 characters."
+              : ""}
+            {inputs.password !== inputs.confirmPassword ? "Passwords do not match." : ""}
+          </p>
         </div>
       </form>
     );

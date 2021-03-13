87049c7034cece263540aa33cacb4f5ea2fea478
Eric Vuu <ericlv91@gmail.com>
2021-03-13 10:36:44 -0800

    pokemon api assignments

---
+++ React/axios-pokemon-api/src/components/Pokemon.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Pokemon = (props) => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const getPokemon = (e) => {
+   axios.get("https://pokeapi.co/api/v2/pokemon?limit=807").then((response) => {
+     setPokemonList(response.data.results);
+   })
+   .catch(error => console.log(error))
+  };
+
+  return (
+    <div className="container-fluid text-center mt-5">
+      <button className="btn btn-lg btn-success" onClick={getPokemon}>
+        Fetch Pokemon
+      </button>
+      <ul className="mt-5 list-unstyled">
+        {
+          pokemonList.map( (pokemon, index) => {
+            return <li key={index}>{pokemon.name}</li>;
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Pokemon;

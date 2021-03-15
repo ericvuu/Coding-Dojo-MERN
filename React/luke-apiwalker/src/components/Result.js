0ba08d7e918b855d165157a4de0e0b8fc0b23c7a
Eric Vuu <ericlv91@gmail.com>
2021-03-15 06:48:06 -0700

    completed with error handling

--- React/luke-apiwalker/src/components/Result.js
+++ React/luke-apiwalker/src/components/Result.js
@@ -5,15 +5,30 @@ const Result = (props) => {
 
   const { searchInfo, setSearchInfo } = props;
   const [responseData, setResponseData] = useState('');
+  const [responseError, setResponseError] = useState(false);
 
   useEffect(() => {
-    axios.get(`http://swapi.dev/api/${searchInfo.category}/${searchInfo.id}/`).then((response) => {
-      setResponseData(response.data);
-    });
+    axios
+      .get(`http://swapi.dev/api/${searchInfo.category}/${searchInfo.id}/`)
+      .then((response) => {
+        setResponseData(response.data);
+        setResponseError(false);
+      })
+      .catch((error) => {
+        setResponseError(true);
+      });
   }, [searchInfo])
 
   return (
     <div>
+
+      {responseError ?
+        <>
+        <h2 className="display-3 text-center mt-3">"These aren't the droids you're looking for" </h2>
+        <img className="img-fluid" src={`${process.env.PUBLIC_URL}/assets/obi.jpg`}  alt="logo"/>
+        </>
+        :
+
       <ul>
         {Object.entries(responseData).map(([key, value]) => (
           <li className="list-unstyled mt-1">
@@ -22,6 +37,8 @@ const Result = (props) => {
           </li>
         ))}
       </ul>
+
+      }
     </div>
   );
 };
